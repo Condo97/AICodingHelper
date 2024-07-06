d090282e933d95f5c7c7c1db621c4416ffa6384e
Alex C <acoundou@gmail.com>
2024-07-06 19:49:38 -0400

    Another good time for a commit :)

--- AICodingHelper/Util/CodeGenerationPlanExecutor.swift
+++ AICodingHelper/Util/CodeGenerationPlanExecutor.swift
@@ -10,8 +10,8 @@ import Foundation
 
 class CodeGenerationPlanExecutor {
     
-    private static let editSystemMessage = "You are an AI coding helper service in an IDE so you must format all your responses in code that would be valid in an IDE. Do not include ```LanguageName or ``` to denote code. You only respond with code that is valid in that language. You only respond to the one requested file. All files will be provided in turn, so therefore you will respond to each individually to preserve correct formatting to the IDE since it is looking to receive one file."
-    
+    private static let editSystemMessage = "PLEASE NOTE YOU ARE PERFORMING AN EDIT TASK FOR ONLY THE GIVEN FILE. You may not add other files to it, you will be asked to generate those other files at a later point. You are an AI coding helper service in an IDE so you must format all your responses in code that would be valid in an IDE. Do not include ```LanguageName or ``` to denote code. You only respond with code that is valid in that language. You only respond to the one requested file. All files will be provided in turn, so therefore you will respond to each individually to preserve correct formatting to the IDE since it is looking to receive one file."
+
     static func generateAndRefactor(authToken: String, plan: CodeGenerationPlan) async throws {
         try await generateAndRefactor(
             authToken: authToken,
